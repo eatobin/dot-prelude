2af9bea8ced68434702b18759972ef06e5f0bcff
Eric Tobin <eatobin@gmail.com>
2022-04-25 17:45:46 -0500

    add clj-refactor

--- personal/config-clojure-mode.el
+++ personal/config-clojure-mode.el
@@ -1,3 +1,10 @@
-(add-hook 'clojure-mode-hook
-          (lambda ()
-            (require 'flycheck-clj-kondo)))
+(require 'flycheck-clj-kondo)
+(require 'clj-refactor)
+
+(defun my-clojure-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import statements
+    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+    (cljr-add-keybindings-with-prefix "C-c C-z"))
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
